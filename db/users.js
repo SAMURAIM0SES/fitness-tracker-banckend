f4cc7a72c32e3db319c806db87aa38b3744ff45a
Gabriel <gabrieljch929@gmail.com>
2022-07-25 13:47:05 -0500

    Working on get user

--- db/users.js
+++ db/users.js
@@ -16,19 +16,41 @@ async function createUser({ username, password }) {
   }  
     
   
-
-
-async function getUser({ username, password }) {
-
+async function getUser({ username, password }) 
+{ if (!username || !password) {
+  return
+}
+try {
+  const user = await getUserByUsername(username)
+if (user && user.password === password) delete user.password; 
+return user;
+// const user = await getUserByUsername(username)
+if (user && user.password !== password) 
+return 
+}
+  catch (error) {
+  throw error
+}
 }
 
 async function getUserById(userId) {
 
 }
 
-async function getUserByUsername(userName) {
+async function getUserByUsername(userName) 
 
-}
+    {
+      const { rows: [user] } = await client.query(`
+        SELECT *
+        FROM users
+        WHERE username=$1;
+      `, [userName]);
+  
+      return user;
+   
+  
+  }
+
 
 module.exports = {
   createUser,
